8efa8aef2a777ad8bc3748df0d692bc72df18e60
Bruno Leão <bruno.melosh@gmail.com>
2023-11-03 17:29:55 -0400

    Início Exercício 5

---
+++ 02-Exercicios de estrutura-condicional/Resolução dos Exercícios/src/Exercício5.java
@@ -0,0 +1,6 @@
+public class Exercício5 {
+    public static void main(String[] args) {
+     System.out.println("*****Valor a pagar pelo produto*****\n");
+     System.out.print("*****Produtos disponíveis******\nCODIGO    ESPECIFICAÇÃO    PREÇO\n1        Cachorro quente   R$ 4,00\n2        X-Salada          R$ 4,50\n3        X-Bacon           R$ 5,00\n4"); 
+    }
+}
