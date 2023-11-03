96d7d26c052240729dfd5a240e6904d46ea87413
Bruno Leão <bruno.melosh@gmail.com>
2023-11-03 16:50:53 -0400

    Resolução exercício 4

--- 02-Exercicios de estrutura-condicional/Resolução dos Exercícios/src/Exercicio4.java
+++ 02-Exercicios de estrutura-condicional/Resolução dos Exercícios/src/Exercicio4.java
@@ -4,15 +4,25 @@ import java.util.Scanner;
 public class Exercicio4 {
 
     public static void main(String[] args) {
-
-
-
-
+        System.out.println("*****Duração do jogo*****\n");
+        System.out.print("Hora inicial da jogo: ");
         Scanner hora = new Scanner(System.in);
+        int x = hora.nextInt();
+        System.out.print("\nHora final da jogo: ");
+        int y = hora.nextInt();
 
+        if (x > y){
+            System.out.printf("\n\nO jogo durou: %d horas\n\n",(y+24)-x);
+        }
+        else if(x == y) {
+            System.out.printf("\n\nO jogo durou: 24 horas\n\n");
+        }
+        else {
+            System.out.printf("\n\nO jogo durou: %d horas\n\n",y-x);
 
+        }
 
-        hora.close();
+       hora.close();
 
     }
 }
