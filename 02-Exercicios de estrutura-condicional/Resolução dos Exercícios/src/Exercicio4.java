cacbe39151acbb3aef766f976f65c8180e590d44
Bruno Leão <bruno.melosh@gmail.com>
2023-11-02 21:41:05 -0400

    inicio do exercicio 4

---
+++ 02-Exercicios de estrutura-condicional/Resolução dos Exercícios/src/Exercicio4.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+
+public class Exercicio4 {
+
+    public static void main(String[] args) {
+
+
+
+
+        Scanner hora = new Scanner(System.in);
+
+
+
+        hora.close();
+
+    }
+}
