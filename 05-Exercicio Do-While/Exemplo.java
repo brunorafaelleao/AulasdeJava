535bbe7d5dd4ddbf8ee870d55bda13fc0a33ca31
Bruno Leão <bruno.melosh@gmail.com>
2023-11-08 09:15:27 -0400

    atualização do-while

--- 05-Exercicio Do-While/Exemplo.java
+++ 05-Exercicio Do-While/Exemplo.java
@@ -4,16 +4,17 @@ public class Exemplo {
    public static void main(String[] args) {
     System.out.print("****Conversão Celsius-Fahrenheit****\n\n");
     Scanner temp = new Scanner(System.in);
-    String resp = "sim";
+    String resp;
 
     do { 
-        System.out.print("Digite o valor a ser convertido: ");
+        System.out.print("\nDigite o valor a ser convertido: ");
         double c = temp.nextDouble();
         double f = 9.0* c / 5 + 32;
         System.out.printf("\nO valor equivalente em Fahrenheut: %.1f\n",f);
         System.out.print("\nDeseja repetir a ação (sim/não)? ");
+        resp = temp.next();
 
-    } while (resp != "não");
+    } while (resp.equals("sim"));
 
     temp.close();
 
