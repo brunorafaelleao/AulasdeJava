c1309d4d660143eb670d98f5a2d2d5265060c4c5
Bruno Leão <bruno.melosh@gmail.com>
2023-11-09 23:13:32 -0400

    andamento do exercicio1 de poo

--- aulaPoo1/src/App.java
+++ aulaPoo1/src/App.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+import entidade.Retangulo;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        System.out.println("*****Cálculo da área, perímetro e diagonal de um retângulo*****\n\n");
+        System.out.print("Digite o valor da altura do retângulo: ");
+
+        Scanner ret = new Scanner(System.in);
+        Retangulo x, y;
+        x = new Retangulo();
+        y = new Retangulo();
+
+        x.altura = ret.nextDouble();
+
+        
+        System.out.printf("A altura do retângulo é: %.1f\n",x.altura);
+
+        ret.close();
     }
 }
---
+++ aulaPoo1/src/entidade/Retangulo.java
@@ -0,0 +1,11 @@
+package entidade;
+
+public class Retangulo {
+    public double largura;
+    public double altura;
+
+    public double areaRet(){
+        double area = altura * largura;
+        return area;
+    }
+    }
